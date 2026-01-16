1e2ac7cac609e983da83b21fa9a2f6c8371870ac
OMAR GALLARDO VALDEZ <212310281@lerdo.tecnm.mx>
2026-01-15 18:38:14 -0600

    Remover node modules

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,6 +6,34 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
     <a href="/admin/index.php" class="boton boton-verde">Volver</a>
+
+    <form class="formulario">
+        <fieldset>
+            <legend>Informacion General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" placeholder="Precio de la propiedad">
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion">Descripcion:</label>
+            <textarea name="" id="descripcion" placeholder="Descripcion de la propiedad"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Informacion de la Propiedad</legend>
+
+
+            <label for="habitaciones">Numero de Habitaciones</label>
+            <input type="number" for="habitaciones">
+
+            
+        </fieldset>
+    </form>
 </main>
 
 
